65ca6e66d0c269e310841c89a28a981819e129bd
kanemototomoki <kanemototomoki@gmail.com>
2024-06-05 18:19:05 +0900

    add: blockNote

--- src/app/_components/BlockNote/index.tsx
+++ src/app/_components/BlockNote/index.tsx
@@ -1,9 +1,18 @@
 'use client'
 
+import "@blocknote/core/fonts/inter.css";
+import {useCreateBlockNote} from "@blocknote/react";
+import {BlockNoteView} from "@blocknote/mantine";
+import "@blocknote/mantine/style.css";
+
 export const BlockNote = () => {
+  const editor = useCreateBlockNote();
+
   return (
     <>
       <h1>Block Note</h1>
+      <BlockNoteView editor={editor}/>
     </>
   )
 }
+
--- src/app/_components/Tabs/index.tsx
+++ src/app/_components/Tabs/index.tsx
@@ -9,9 +9,9 @@ export const Tabs = () => {
   return (
     <OriginalTabs aria-label="block editor tabs">
       <TabList className="flex gap-4 [&>*]:border-2 [&>*]:p-2 aria-selected:[&>*]:bg-sky-200">
+        <Tab id="blocknote">Block Note</Tab>
         <Tab id="gutenberg">Gutenberg</Tab>
         <Tab id="editorjs">Editor.js</Tab>
-        <Tab id="blocknote">Block Note</Tab>
       </TabList>
       <TabPanel id="gutenberg">
         <Gutenberg/>
