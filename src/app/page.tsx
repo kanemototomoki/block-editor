1ddf92f984bc24177bef79747a6c3f1a8bf99a9f
kanemototomoki <kanemototomoki@gmail.com>
2024-06-05 18:38:09 +0900

    change: dynamic import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
-import {Tabs} from "@/app/_components/Tabs";
-import {Provider} from "@/app/Provider";
+'use client'
+
+import dynamic from 'next/dynamic'
+
+const DynamicTabs = dynamic(() => import('./_components/Tabs').then((module) => module.Tabs), {
+  ssr: false,
+})
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Provider>
-        <Tabs/>
-      </Provider>
+      <DynamicTabs/>
     </main>
   );
 }
